6e0fdb7680b3d453442029bb3ab8ed0fc1bdf851
Yu-Hayate <112352934+Yu-Hayate@users.noreply.github.com>
2023-11-28 10:00:36 -0500

    Mise à jour pxt.json, main.blocks, Mouse.ts

--- Mouse.ts
+++ Mouse.ts
@@ -1,3 +1,7 @@
+namespace SpriteKind {
+    export const Mouse = SpriteKind.create()
+}
+
 let sensibility = 1.124 * 1023
 let Y = 0
 let X = 0
@@ -8,21 +12,12 @@ let MouseImage = img`
     . 2 . 
     `
 let mouseHitbox: number[] = [0,0]
+let MouseDATA = sprites.create(MouseImage, SpriteKind.Player)
+MouseDATA.destroy()
 let hitboxes: { name: string, rect: number[] }[] = [];
 enum Position {
     X = 0,
     Y = 1
-}
-function drawImageAtMouse(image: Image): void {
-    // Clear the screen
-    screen.fill(0);
-
-    // Calculate the top-left position of the image based on the mouse hitbox center
-    let imageX = mouseHitbox[0] - image.width / 2;
-    let imageY = mouseHitbox[1] - image.height / 2;
-
-    // Draw the image at the calculated position
-    screen.drawImage(image, imageX, imageY);
 }
 function isPositionInHitbox(x: number, y: number): string {
     x -= mouseHitbox[0]
@@ -35,29 +30,35 @@ function isPositionInHitbox(x: number, y: number): string {
     }
     return "";
 }
-game.onUpdate(function() {
-    if (DrawMouseOnScreen) {
-        drawImageAtMouse(MouseImage)
-    }
-})
 //% color="#8A2BE2" icon="\uf245" 
 namespace Mouse {
+    game.onUpdate(function() {
+        if (DrawMouseOnScreen){
+            MouseDATA.setPosition(Mouse.x() , Mouse.y())
+        }
+    })     
     //% block="Get $Pos of mouse"
-export function GetPositionOfMouse(Pos: Position) {
-    if (Pos == 0) {
-        X = Math.map(Math.map(controller.acceleration(ControllerDimension.X), -1023, 1023, 0 - sensibility, sensibility), -1023, 1023, 0, scene.screenWidth()) - mouseHitbox[0]
-        return X
-    } else {
-        Y = Math.map(Math.map(controller.acceleration(ControllerDimension.Y), -1023, 1023, 0 - sensibility, sensibility), -1023, 1023, 0, scene.screenHeight()) - mouseHitbox[1]
-
-        return Y
+    export function GetPositionOfMouse(Pos: Position) {
+        if (Pos == 0) {
+            X = Math.map(Math.map(controller.acceleration(ControllerDimension.X), -1023, 1023, 0 - sensibility, sensibility), -1023, 1023, 0, scene.screenWidth()) - mouseHitbox[0]
+            return X
+        } else {
+            Y = Math.map(Math.map(controller.acceleration(ControllerDimension.Y), -1023, 1023, 0 - sensibility, sensibility), -1023, 1023, 0, scene.screenHeight()) - mouseHitbox[1]
+            return Y
+        }
     }
-}
+
 //% block="Set sensibility of mouse to $sensibilityNum"
 //% sensibilityNum.def=1.12
 export function Setsensibility(sensibilityNum: number) {
     sensibility = Math.map(sensibilityNum, 0, 1, 500, 1023)
 }
+    export function x() {
+        return GetPositionOfMouse(Position.X)
+    } 
+    export function y() {
+        return GetPositionOfMouse(Position.Y)
+    }
     //% block="create hitbox with name $name at x$x1 y$y1 to x$x2 y$y2"
     //% Name.shadow="HitboxNameList"
     export function createHitbox(x1: number, y1: number, x2: number, y2: number, name: string): void {
@@ -77,9 +78,11 @@ export function Setsensibility(sensibilityNum: number) {
     }
     //% block="draw mouse on screen $draw with image $img=screen_image_picker and hitbox at x$x1 y$y1"
     export function DrawMouse(draw: boolean, img: Image, x1: number, y1: number) {
+        MouseDATA.destroy()
         DrawMouseOnScreen = draw
         MouseImage = img
         mouseHitbox = [x1,y1]
+        MouseDATA = sprites.create(MouseImage, SpriteKind.Player)
     }
     //% block="$name"
     //% blockId=HitboxNameList
